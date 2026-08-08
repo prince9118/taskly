0461b8144235462f183cad0067aee321b19e1a17
prince9118 <pchaudharyskn@gmail.com>
2026-08-09 01:10:33 +0530

    cli program

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log(process.argv);
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("taskly")
+  .description("A simple CLI task manager")
+  .version("1.0.0");
+
+program
+  .command("split")
+  .description("Add the task in the  InMemory")
+  .option("--add", "Add task")
+  .option("--remove", "remove the task")
+  .action((str, option) => {
+    console.log();
+  });
+program.parse();
