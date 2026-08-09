20e43cfcafa82ddd243cd5991b911e16f0d92d0c
prince9118 <pchaudharyskn@gmail.com>
2026-08-09 17:48:11 +0530

    storage

--- src/storage/task.storage.ts
+++ src/storage/task.storage.ts
@@ -1,1 +1,17 @@
-import { readFile,writeFile} from "node:fs/promises";
+import { readFile, writeFile } from "node:fs/promises";
+import type { Task } from "../types/task.js";
+
+const filePath = "./data/task.json";
+
+export async function getTasks(): Promise<Task[]> {
+  try {
+    const data = await readFile(filePath, "utf-8");
+    return JSON.parse(data) as Task[];
+  } catch {
+    return [];
+  }
+}
+
+export async function saveTasks(tasks: Task[]): Promise<void> {
+  await writeFile(filePath, JSON.stringify(tasks, null, 2), "utf-8");
+}
