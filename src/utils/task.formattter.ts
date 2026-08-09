381a5e3bda6495fcabdc7ac4e5dc16652d1e97da
prince9118 <pchaudharyskn@gmail.com>
2026-08-09 23:41:07 +0530

    formattor

--- src/utils/task.formattter.ts
+++ src/utils/task.formattter.ts
@@ -1,5 +1,5 @@
 import type { TaskType } from "../types/task.js";
 export function formatTask(task: TaskType): string {
   const status = task.completed ? "✓" : "○";
-  return `${task.id}  ${status} ${task.priority}  ${task.title}`;
+  return `${task.id}   ${status}      ${task.priority}      ${task.title}`;
 }
