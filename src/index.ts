9cc3eb05494c205cd810bde003e9ddaacbbee208
prince9118 <pchaudharyskn@gmail.com>
2026-08-10 02:40:40 +0530

    done command update

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import { formatTask } from "./utils/task.formattter.js";
 import {
   createTask,
   getAllTasks,
-  completeTask
+  completeTask,
+  removeTask,
+  updateTask
 } from "./services/task.service.js";
 import { getTasks } from "./storage/task.storage.js";
 import inquirer from "inquirer";
@@ -38,25 +40,6 @@ program
   });
 
 program
-  .command("get")
-  .description("Get all the tasks")
-  .action(async (task, options) => {
-    const answers = await inquirer.prompt([
-      {
-        type: "input",
-        name: "title",
-        message: "what is your task?"
-      },
-      {
-        type: "select",
-        name: "priority",
-        message: "Select priority",
-        choices: ["low", "medium", "high"]
-      }
-    ]);
-  });
-
-program
   .command("list")
   .description("List all tasks")
   .action(async () => {
@@ -70,10 +53,23 @@ program
 program
   .command("done")
   .description("Mark task as completed")
-  .argument("<id>", "Task Id")
-  .action(async (id) => {
+  .action(async () => {
     try {
-      const taskId = parseTaskId(id);
+      const tasks = await getAllTasks();
+      if (!tasks) {
+        throw new Error("No task found");
+      }
+      const { taskId } = await inquirer.prompt([
+        {
+          type: "select",
+          name: "taskId",
+          message: "Select task to completd ?",
+          choices: tasks.map((task) => ({
+            name: `${task.id} - ${task.title} `,
+            value: task.id
+          }))
+        }
+      ]);
       const task = await completeTask(taskId);
       console.log(`Task ${task.id} marked as completed`);
     } catch (error) {
@@ -81,4 +77,67 @@ program
     }
   });
 
+program
+  .command("remove")
+  .description("Remve task")
+  .argument("<id>")
+  .action(async (id) => {
+    try {
+      const taskId = parseTaskId(id);
+      const task = await removeTask(taskId);
+      console.log(`Task ${task.id} removed`);
+    } catch (error) {
+      throw new Error("Soemthing wrong");
+    }
+  });
+
+program
+  .command("set")
+  .description("Update the tasks")
+  .action(async () => {
+    const tasks = await getAllTasks();
+    if (tasks.length === 0) {
+      console.log("No Task Available");
+      return;
+    }
+    const { taskId } = await inquirer.prompt([
+      {
+        type: "select",
+        name: "taskId",
+        message: "Which task do you want to update ?",
+        choices: tasks.map((task) => ({
+          name: `${task.id} - ${task.title} `,
+          value: task.id
+        }))
+      }
+    ]);
+    const task = tasks.find((task) => task.id === taskId);
+    if (!task) {
+      throw new Error("Task not found");
+    }
+    const answers = await inquirer.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "what is your task title?",
+        default: task.title
+      },
+      {
+        type: "confirm",
+        name: "completed",
+        message: "Is the task completed",
+        default: task.completed
+      },
+      {
+        type: "select",
+        name: "priority",
+        message: "Select Priority",
+        choices: ["low", "medium", "high"],
+        default: task.priority
+      }
+    ]);
+    const updatedTask = await updateTask(taskId, answers);
+    console.log("Task updated successfully");
+    console.log(updatedTask);
+  });
 program.parse();
--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -45,3 +45,36 @@ export async function completeTask(id: number): Promise<TaskType> {
   await saveTasks(tasks);
   return task;
 }
+
+export async function removeTask(id: number): Promise<TaskType> {
+  const tasks = await getTasks();
+  const index = tasks.findIndex((task) => task.id === id);
+  if (index === -1) {
+    throw new Error(`Task with id ${id} not found`);
+  }
+  const deletedTask = tasks[index]!;
+  tasks.splice(index, 1);
+  //   const [deletedTask] = tasks.splice(index, 1);
+  await saveTasks(tasks);
+  return deletedTask;
+}
+
+export async function updateTask(id:number,
+    updates:{
+        title:string;
+        completed:boolean;
+        priority:TaskPriority;
+
+    }):Promise<TaskType>{
+        const tasks=await getTasks();
+        const task=tasks.find((task)=>task.id === id);
+        if(!task){
+            throw new Error(`Task with id ${id} not found`);
+        }
+        task.title=updates.title;
+        task.completed=updates.completed;
+        task.priority=updates.priority;
+        await saveTasks(tasks);
+        return task;
+
+    }
