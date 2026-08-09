ab3ea1a40570b29323352f4e0e42809b7cc5255d
prince9118 <pchaudharyskn@gmail.com>
2026-08-09 18:16:19 +0530

    task add

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import { title } from "node:process";
-import type { Task } from "./types/task.js";
+import { Command } from "commander";
+import { createTask } from "./services/task.service.js";
 
-const task: Task = {
-  id: 1,
-  title: "Learn Redis",
-  completed: false,
-  priority: "high"
-};
-console.log(task);
+const program = new Command();
+
+program.name("taskly").description("cli task manager").version("1.0.0");
+
+program
+  .command("add")
+  .description("Add a new task")
+  .argument("<task>")
+  .option("--p, --priority <level>", "Task priority")
+  .action(async (task, options) => {
+    const createdTask = await createTask(task, options.priority ?? "medium");
+    console.log("Task created");
+    console.log(createdTask);
+  });
+
+program.parse();
